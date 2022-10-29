fd7b8c0b92128027b42ed0d3473e437166926852
Maxim_Pushkin <psbmm@yandex.ru>
2022-10-29 16:02:53 +0300

    Exampl 009 01

--- Exampl009/Program.cs
+++ Exampl009/Program.cs
@@ -102,7 +102,6 @@ int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
 void PrintArray(int[] array)
 {
     int count = array.Length;
-
     for (int i = 0; i < count; i++)
     {
         Console.Write($"{array[i]} ");
