d261b2d2f2f034c7749412651075e2a079d2ce7c
Maxim_Pushkin <psbmm@yandex.ru>
2022-11-03 21:47:35 +0300

    Exampl 10

---
+++ Exampl010/Program.cs
@@ -0,0 +1,31 @@
+﻿/*//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while (current <= n)
+{
+    Console.Write(current + ", ");
+    current = current + 2;    
+}
+*/
+
+
+
+
+void ProgressStep(int num)
+{
+for(int i = 0; i<num;)
+{
+    i++;
+    Console.Write($"{i} ");
+}
+}
+
+Console.WriteLine("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ProgressStep(n);
+
+
+
+
+
