4fd2cf5144c5a6da2ae6d485203b5382d8ab6381
Maxim_Pushkin <psbmm@yandex.ru>
2022-11-11 19:46:40 +0300

    Задача на подсчет количества четных элементов массива

--- Exampl012/Program.cs
+++ Exampl012/Program.cs
@@ -1,8 +1,49 @@
 ﻿//Урок 5. Функции продолжение
 
 //Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// Генерация массива
+int[] CreateRandomeArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size]; // инициализация миссива размера size
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+return array;
+}
 
+// вывод массива в терминале
+void ShowArray(int[] array)
+{
+    for(int i =0; i < array.Length; i++)
+        Console.Write(array[i]+" ");
+    Console.WriteLine();    
+}   
 
+//Запрос элементов массива
+Console.WriteLine("Input a number of elemrnts: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a min posible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a max posible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+//Запуск метода генерации и вывода элементов массива
+int[] myArray = CreateRandomeArray(n, min, max);
+ShowArray(myArray);
+
+//кол-во четных чисел в массиве
+int EvenNambers(int[] array)
+{
+    int count = 0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i] % 2==0)
+        count ++;
+    }
+    return count;
+}
+Console.WriteLine(EvenNambers(myArray));
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях (индексах).
 
 
