ae52ebeac9daf6af314e2fce9b3d4bfa0348f57e
Maxim_Pushkin <psbmm@yandex.ru>
2022-11-19 20:02:14 +0300

    уравнение прямых, нах координат точки пересечения прямых

--- Exampl014/Program.cs
+++ Exampl014/Program.cs
@@ -30,20 +30,20 @@ Console.WriteLine($"result a number > 0 is {Kolich (m)}");
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-/*void InterectionPoint(double b1, double k1, double b2, double k2)
+void InterectionPoint(double c1, double a1, double c2, double a2)
 {
-    double x = (b2 - b1) / (k1 - k2);
-    double y = k1*x + b1;
+    double x = (c2 - c1) / (a1 - a2);
+    double y = a1*x + c1;
     Console.WriteLine(x + ", " + y);
 } 
 
-Console.Write("Input k1 : ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input k2 : ");
-double k2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input b1 : ");
 double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k1 : ");
+double k1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input b2 : ");
 double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k2 : ");
+double k2 = Convert.ToDouble(Console.ReadLine());
 InterectionPoint(b1, k1, b2, k2);
-*/
+
