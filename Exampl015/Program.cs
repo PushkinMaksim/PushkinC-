9d278c5e10451636659d9d28e84320b24e142351
Maxim_Pushkin <psbmm@yandex.ru>
2022-11-22 07:12:04 +0300

    Задачи к семинару 7, за исключением последней задачи

--- Exampl015/Program.cs
+++ Exampl015/Program.cs
@@ -39,6 +39,41 @@ Show2dArray(myArray);
 //Урок 7. Как не нужно писать код. Часть 1
 //Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 //Объявление массива
+/*
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)         //переход по строкам
+    {
+        for(int j = 0; j < columns; j++)  //переход по столбцам
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j =0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a min possible: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a max possible: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
 
 double[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
@@ -82,7 +117,7 @@ Show2dArray(myArray);
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в 
 //двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 //Генерация двухмерного массива случ числами
-
+/*
 int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] array = new int[rows, columns];
@@ -122,14 +157,10 @@ Show2dArray(myArray);
 void Element(int [,] array, int g, int k)
 {
     for(int i = 0; i < array.GetLength(0); i++)
-    {
         for(int j = 0; j < array.GetLength(1); j++)
             if(g == i && k == j) Console.Write($"element value: {array[i, j]}");
-            else
-            {
-                //Console.Write("No "); //закоммитил т.к. всь массив перебрирает и что не нашел, тоже выводит
-            }
-    }
+if(g > array.GetLength(0) || k > array.GetLength(1))
+                Console.WriteLine("No element");
 }
 
 Console.WriteLine();
@@ -138,19 +169,25 @@ int g = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input index columns j: ");
 int k = Convert.ToInt32(Console.ReadLine());
 Element(myArray, g, k);
-
-
-
-
+*/
 
 //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
 //элементов в каждом столбце.
 
-//Например, задан массив:
-//1 4 7 2
-//5 9 2 3
-//8 4 2 4
-//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
-
-
+void Arifm(int[,] array)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        int sum = 0;
+        for(int i =0; i < array.GetLength(0); i++)
+        {
+            sum = 0;
+            sum = sum + array[i,j];
+            double result = sum / array.GetLength(0);
+            Console.Write(result);
+            Console.WriteLine();
+        }
+    }
+}
+Console.WriteLine();
+Arifm(myArray);
