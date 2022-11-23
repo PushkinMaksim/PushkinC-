a535db0fb1101155f0df5735db13be41a152129e
Maxim_Pushkin <psbmm@yandex.ru>
2022-11-24 00:11:27 +0300

    Добавлено решение последней задачи

--- Exampl015/Program.cs
+++ Exampl015/Program.cs
@@ -1,5 +1,6 @@
-﻿//Генерация двухмерного массива случ числами
-/*
+﻿/*
+//Генерация двухмерного массива случ числами
+
 int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] array = new int[rows, columns];
@@ -35,12 +36,12 @@ int max = Convert.ToInt32(Console.ReadLine());
 int[,] myArray = CreateRandom2dArray(m, n, min, max);
 Show2dArray(myArray);
 */
-/*
+
 //Урок 7. Как не нужно писать код. Часть 1
 //Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 //Объявление массива
 /*
-int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+//////int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] array = new int[rows, columns];
     for(int i = 0; i < rows; i++)         //переход по строкам
@@ -73,8 +74,10 @@ Console.WriteLine("Input a max possible: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
 int[,] myArray = CreateRandom2dArray(m, n, min, max);
-Show2dArray(myArray);
+//////Show2dArray(myArray);
+*/
 
+/*
 double[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     double[,] array = new double[rows, columns];
@@ -84,7 +87,7 @@ double[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
         {
             array[i, j] = new Random().Next(minValue, maxValue); //замена целочисленных элементов на вещественные
             double num1 = new Random().NextDouble();
-            double num2 = Math.Round(num1, 2);
+            double num2 = Math.Round(num1, 1);
             array[i, j] = array[i, j] + num2;
         }
     }
@@ -171,23 +174,64 @@ int k = Convert.ToInt32(Console.ReadLine());
 Element(myArray, g, k);
 */
 
+/*
 //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
 //элементов в каждом столбце.
 
-void Arifm(int[,] array)
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
-    for(int j = 0; j < array.GetLength(1); j++)
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)         
     {
-        int sum = 0;
-        for(int i =0; i < array.GetLength(0); i++)
+        for(int j = 0; j < columns; j++)  
         {
-            sum = 0;
-            sum = sum + array[i,j];
-            double result = sum / array.GetLength(0);
-            Console.Write(result);
-            Console.WriteLine();
+            array[i, j] = new Random().Next(minValue, maxValue);
         }
     }
+    return array;
 }
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j =0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a min possible: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a max possible: ");
+int max = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
-Arifm(myArray);
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+
+double[,] ArithmeticMean(int[,] array)
+{
+    double[,] newArray = new double[array.GetLength(1), array.GetLength(0)];
+    double result = 0;
+    int sum = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                sum = sum + array[i, j];
+                result = sum / array.GetLength(0);
+            } 
+            Console.Write(result + " ");
+            sum = 0;
+            result = 0;
+        }
+    return newArray;       
+}
+
+Console.WriteLine();
+ArithmeticMean(myArray);
+*/
